6cf414c6b560a46d9f8a23541353038ef26d9173
michi <chischte@gmx.ch>
2020-10-27 20:56:04 +0100

    Add first tests

---
+++ App1/CalculatorTest/CalculatorTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        private readonly Calculator testee;
+
+        public CalculatorTests()
+        {
+            this.testee = new Calculator();
+        }
+
+        [TestMethod]
+        public void GetResults_One_Plus_One_ReturnsCorrectResult()
+        {
+            // Act
+            var result = this.testee.Add(3, 5);
+
+            // Assert
+            Assert.AreEqual(8, result);
+        }
+        [TestMethod]
+        public void GetResults_Five_Minus_Three_returnsCorrectResult()
+        {
+            // Act
+            var result = this.testee.Subtract(5, 3);
+
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void GetResults_Five_Times_Six_returnsCorrectResult()
+        {
+            // Act
+            var result = this.testee.Multiply(5, 6);
+
+            // Assert
+            Assert.AreEqual(30, result);
+        }
+
+        [TestMethod]
+        public void GetResults_30_diveded_by_6_returnsCorrectResult()
+        {
+            // Act
+            var result = this.testee.Divide(30, 6);
+
+            // Assert
+            Assert.AreEqual(5, result);
+        }
+
+
+    }
+}
