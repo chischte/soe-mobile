d13add677d5daff0b16df8f9695d8c1ade671cac
michi <chischte@gmx.ch>
2020-11-15 20:42:26 +0100

    BKP

--- Calculator/Calculator.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ Calculator/Calculator.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"1020fd78-b2ae-437d-b2f9-1e79d1b54a62"
+	.asciz	"8cdc5880-b529-4a7b-a316-1cc896122ff5"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
